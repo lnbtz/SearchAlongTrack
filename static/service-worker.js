5020fd78775ade82f0efd8677d9c2b5d877407a4
Leon Beitz <leon.beitz@haw-hamburg.de>
2025-04-19 21:26:02 +0200

    created manifest.json and service-worker.js learn what they do and check https://dev.to/braide/building-progressive-web-applications-using-sveltekit-58gj

---
+++ static/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "svelte-app-cache-v1";
+const urlsToCache = ["/", "/index.html", "/global.css", "/build/bundle.js"];
+
+// Install event
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
